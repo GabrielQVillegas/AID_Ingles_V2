1700f2870480e526e4ad196da27b5647a63667ac
GabrielQVillegas <gabriel_3d2y@hotmail.com>
2026-03-25 22:58:04 -0600

    AID V2.1

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 const CACHE="aid-ingles-v2.1";
-const ASSETS=["./","./index.html","./manifest.json","./service-worker.js","./palabras_vocabulario_definitivo.json","./icon-192_V1.png","./icon-512_V1.png"];
+const ASSETS=["./","./index.html","./manifest.json","./service-worker.js","./palabras_vocabulario_definitivo.json","./icon-192_V2.png","./icon-512_V2.png"];
 
 self.addEventListener("install",e=>{
 e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
